ed67dcc447704bec6d3cb04e59bb90cf13970b26
FlorianDoublet <florian.doublet@hotmail.fr>
2017-02-23 15:23:00 +0100

    Add some migrations to add several field

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.delete_all
+
+Book.create(title: "Harry Potter", author: "J.K Rolling", description: "A young wizzard with a magical scar.",
+  rating:3, finished_on: 10.days.ago)
+Book.create(title: "Amazing Florian", author: "Florian Doublet", description: "Story of an amazing man.",
+  rating: 5, finished_on: 10.days.ago)
+
+p "Created #{Book.count} books"
